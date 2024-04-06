4f95f54a7436dde4782116acbddbb7b46752829d
Ashish Gaur <ashishgaurp91@gmail.com>
2024-04-06 12:17:41 +0530

    Configure simplecov

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,2 +1,24 @@
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails'do
+  # Configuration options go here
+  add_filter %w[
+    app/views
+    app/channels
+    app/jobs/application_job.rb
+    app/mailers/application_mailer.rb
+    app/helpers/application_helper.rb
+    lib/rails
+    lib/templates
+    bin
+    coverage
+    log
+    test
+    vendor
+    node_modules
+    db
+    doc
+    public
+    storage
+    tmp
+  ]
+end
